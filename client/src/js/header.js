90df8fe05804186af45f5d99f041d33f3949c2ec
Sean Nzeribe <seannzeribe@Seans-MacBook-Air.local>
2023-03-14 16:00:36 -0700

    heroku deployment

---
+++ client/src/js/header.js
@@ -0,0 +1,12 @@
+const header = `
+/* _____   ____   ____    __
+  |       |      |    |  |  |  |
+  |_____  |____  |____|  |  |  |
+       |  |      |    |  |  |  |
+   ____|  |____  |    |  |  |__|
+
+just another text editor made by sean
+*/                          
+`;
+
+export { header };
